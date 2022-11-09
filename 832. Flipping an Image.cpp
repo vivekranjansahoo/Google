3b0eb74b1fade5da0f821bb86321ca568ad30c41
vivekranjansahoo <97107034+vivekranjansahoo@users.noreply.github.com>
2022-11-09 19:57:32 +0530

    Update 832. Flipping an Image.cpp

--- 832. Flipping an Image.cpp
+++ 832. Flipping an Image.cpp
@@ -1,3 +1,23 @@
+/*
+Example-1:
+--------------------
+
+Input: image = [[1,1,0],[1,0,1],[0,0,0]]
+Output: [[1,0,0],[0,1,0],[1,1,1]]
+Explanation: First reverse each row: [[0,1,1],[1,0,1],[0,0,0]].
+Then, invert the image: [[1,0,0],[0,1,0],[1,1,1]]
+
+Example-2 :
+-----------------
+
+Input: image = [[1,1,0,0],[1,0,0,1],[0,1,1,1],[1,0,1,0]]
+Output: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+Explanation: First reverse each row: [[0,0,1,1],[1,0,0,1],[1,1,1,0],[0,1,0,1]].
+Then invert the image: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+
+*/
+
+
 class Solution {
 public:
     vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
