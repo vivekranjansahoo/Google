a7d4c8fbfe1e324f3617638bb3891c2f0a7b490c
vivekranjansahoo <97107034+vivekranjansahoo@users.noreply.github.com>
2022-11-09 20:00:33 +0530

    Update 1108. Defanging an IP Address.cpp

--- 1108. Defanging an IP Address.cpp
+++ 1108. Defanging an IP Address.cpp
@@ -1,3 +1,18 @@
+/*
+Example-1 : 
+-----------------------
+
+Input: address = "1.1.1.1"
+Output: "1[.]1[.]1[.]1"
+
+Example-2:
+---------------
+Input: address = "255.100.50.0"
+Output: "255[.]100[.]50[.]0"
+
+*/
+
+
 class Solution {
 public:
     string defangIPaddr(string address) {
