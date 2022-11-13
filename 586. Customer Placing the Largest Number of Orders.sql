35cc7472752acdd6df9e9873501c97acfa45aed1
vivekranjansahoo <97107034+vivekranjansahoo@users.noreply.github.com>
2022-11-13 15:35:35 +0530

    Create 586. Customer Placing the Largest Number of Orders.sql

---
+++ 586. Customer Placing the Largest Number of Orders.sql
@@ -0,0 +1,46 @@
+/*
+Table: Orders
+
++-----------------+----------+
+| Column Name     | Type     |
++-----------------+----------+
+| order_number    | int      |
+| customer_number | int      |
++-----------------+----------+
+order_number is the primary key for this table.
+This table contains information about the order ID and the customer ID.
+
+Example 1:
+
+Input: 
+Orders table:
++--------------+-----------------+
+| order_number | customer_number |
++--------------+-----------------+
+| 1            | 1               |
+| 2            | 2               |
+| 3            | 3               |
+| 4            | 3               |
++--------------+-----------------+
+Output: 
++-----------------+
+| customer_number |
++-----------------+
+| 3               |
++-----------------+
+Explanation: 
+The customer with number 3 has two orders, which is greater than either customer 1 or 2 because each of them only has one order. 
+So the result is customer_number 3.
+
+*/
+
+
+ mySQL Code
+------------------------------------------------
+
+# Write your MySQL query statement below
+select customer_number 
+from Orders
+group by customer_number
+order by count(order_number) desc
+limit 1
