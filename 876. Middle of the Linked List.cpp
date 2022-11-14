295159a565824b6d488e5df8bd93e1251d3020dc
vivekranjansahoo <97107034+vivekranjansahoo@users.noreply.github.com>
2022-11-14 19:41:55 +0530

    Create 876. Middle of the Linked List.cpp

---
+++ 876. Middle of the Linked List.cpp
@@ -0,0 +1,76 @@
+/*
+Example 1:
+
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
+
+
+Example 2:
+
+Input: head = [1,2,3,4,5,6]
+Output: [4,5,6]
+Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+
+
+*/
+
+//code
+-------------------------------------
+  1st approach
+  ---------------------------------
+  /**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode *temp=head;
+        int count=0;
+        while(temp!=NULL){
+            count++;
+            temp=temp->next;
+        }
+        count=count/2+1;
+        int k=1;
+        while(k<count){
+            k++;
+            head=head->next;
+        }
+        return head;
+    }
+};
+
+-----------------------------------------------------------------
+  2nd Approach
+  ---------------------------------------------------
+  
+ /**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode *slow=head,*fast=head;
+        while(fast && fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
+    }
+};
+--------------------------------------
