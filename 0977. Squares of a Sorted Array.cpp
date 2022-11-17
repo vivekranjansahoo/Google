e0f9c8f7cdcdadd074a14c4868dd4910ecfdd83b
vivekranjansahoo <97107034+vivekranjansahoo@users.noreply.github.com>
2022-11-17 23:29:02 +0530

    Create 0977. Squares of a Sorted Array.cpp

---
+++ 0977. Squares of a Sorted Array.cpp
@@ -0,0 +1,45 @@
+/*
+Example 1:
+
+Input: nums = [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Explanation: After squaring, the array becomes [16,1,0,9,100].
+After sorting, it becomes [0,1,9,16,100].
+Example 2:
+
+Input: nums = [-7,-3,2,3,11]
+Output: [4,9,9,49,121]
+ 
+*/
+
+================================
+  
+//1st Solution
+  
+ class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int>ans;
+        for(int i=0;i<nums.size();i++){
+            ans.push_back(nums[i]*nums[i]);
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
+
+
+
+================================
+  
+//2nd Solution
+  
+ class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        for (auto &i:nums)
+        i=i*i;
+        sort(nums.begin(),nums.end());
+        return nums;
+    }
+};
