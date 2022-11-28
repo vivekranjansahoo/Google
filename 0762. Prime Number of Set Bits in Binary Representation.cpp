fc8605b4acb319005d989d21cbec399fa9b0c1a2
vivekranjansahoo <97107034+vivekranjansahoo@users.noreply.github.com>
2022-11-28 22:00:36 +0530

    Create 0762. Prime Number of Set Bits in Binary Representation.cpp

---
+++ 0762. Prime Number of Set Bits in Binary Representation.cpp
@@ -0,0 +1,25 @@
+class Solution {
+ public:
+  int countPrimeSetBits(int left, int right) {
+    int primes = 0;
+    for (int i = left; i <= right; i++) {
+      int ones = 0;
+      int n = i;
+      while (n) {
+        if (n & 1) ones++;
+        n >>= 1;
+      }
+      if (isPrime(ones)) primes++;
+    }
+    return primes;
+  }
+
+  bool isPrime(int n) {
+    if (n == 1) return false;
+    for (int i = 2; i < n; i++) {
+      if (n % i == 0) return false;
+    }
+
+    return true;
+  }
+};
