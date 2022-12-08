86e1f216fb6e50d38b48048b6ce7a9aee33998ad
vivekranjansahoo <97107034+vivekranjansahoo@users.noreply.github.com>
2022-12-08 18:23:12 +0530

    Create 0872. Leaf-Similar Trees.cpp

---
+++ 0872. Leaf-Similar Trees.cpp
@@ -0,0 +1,35 @@
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        vector<int> leafOne;
+        vector<int> leafTwo;
+        
+        preOrder(root1, leafOne);
+        preOrder(root2, leafTwo);
+        
+        return (leafOne == leafTwo);
+    }
+    
+    void preOrder(TreeNode* root, vector<int>& leafNumbers){
+        
+        if(root){
+            if(root->left == NULL && root->right == NULL){
+                leafNumbers.push_back(root->val);
+            }
+        
+            preOrder(root->left, leafNumbers);
+            preOrder(root->right, leafNumbers);
+        }
+    }
+};
